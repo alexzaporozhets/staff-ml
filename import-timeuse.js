e69e453997c8b5f701e7f1fd6a56f2eedecbac35
Alex Zaporozhets <alexzaporozhets@gmail.com>
2017-03-12 16:54:44 +0200

    testing

--- import-timeuse.js
+++ import-timeuse.js
@@ -95,14 +95,13 @@ mysql.createConnection({
       ['apps', 'websites'].forEach(key => {
         for (let prop in data[key]) {
           connection.execute(
-            'INSERT INTO `timeuse_daily` VALUES (user_id, date, app, website, time) VALUES(?,?,?,?,?) ',
+            'INSERT INTO `timeuse_daily` VALUES (user_id, date, app, website, time) VALUES(?,?,?,?,?)',
             [data.user_id, data.date, (key == 'apps') ? prop : null, (key == 'websites') ? prop : null, data[key][prop]]
           ).catch(error => {
             console.log(error); // Error: Not Found
           });
         }
       });
-      process.exit();
     }
   });
 
