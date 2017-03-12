3dc4e0056924f6d501adf6de1d530887cc1bf419
Alex Zaporozhets <alexzaporozhets@gmail.com>
2017-03-12 18:48:40 +0200

    testing

--- import-timeuse.js
+++ import-timeuse.js
@@ -122,5 +122,5 @@ rl.on('line', function (line) {
 rl.on('close', function () {
   // do something on finish here
   console.log('[Import timeuse] Done:', result, 'days');
-  lastUpdateOperation.then(process.exit());
+  // lastUpdateOperation.then(process.exit());
 });
